122fba892bb0a8f96b2a25425f9c3c430e4ce28e
Hayata <hayata0430@yahoo.co.jp>
2017-05-03 23:14:07 +0900

    JPA関連の設定を修正

--- SpringBatchSample1/src/main/java/com/example/spring/repository/SampleTableRepository.java
+++ SpringBatchSample1/src/main/java/com/example/spring/repository/SampleTableRepository.java
@@ -1,9 +1,6 @@
 package com.example.spring.repository;
 
 import com.example.spring.entity.SampleTableEntity;
-
-import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
 public interface SampleTableRepository extends JpaRepository<SampleTableEntity, Long> {}
