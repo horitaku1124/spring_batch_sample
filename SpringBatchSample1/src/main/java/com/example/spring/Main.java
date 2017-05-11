5706845a8fb6686b65ec1a3fb951044d6671c972
Hayata <hayata0430@yahoo.co.jp>
2017-05-11 23:38:58 +0900

    SpringBootに修正

--- SpringBatchSample1/src/main/java/com/example/spring/Main.java
+++ SpringBatchSample1/src/main/java/com/example/spring/Main.java
@@ -1,27 +1,24 @@
 package com.example.spring;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobExecutionException;
-import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        {
-            JobLauncher jobLauncher = context.getBean("jobLauncher", JobLauncher.class);
-            Job job = context.getBean("myJob1", Job.class);
-
-            JobExecution execution = jobLauncher.run(job, new JobParameters());
-            System.out.println("Job Exit Status : " + execution.getStatus());
-        }
+        SpringApplication.run(Main.class, args);
+//        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        {
+//            JobLauncher jobLauncher = context.getBean("jobLauncher", JobLauncher.class);
+//            Job job = context.getBean("myJob1", Job.class);
+//
+//            JobExecution execution = jobLauncher.run(job, new JobParameters());
+//            System.out.println("Job Exit Status : " + execution.getStatus());
+//        }
     }
 }
