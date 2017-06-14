24d2f175d162bbe6638b2b6c553fc4fdc0c166a4
Takuya Horimatsu <new_woory@hotmail.com>
2017-06-14 22:20:50 +0900

    batch sample 1

--- SpringBatchSample2/src/test/java/com/example/spring/SpringBatchSample2ApplicationTests.java
+++ SpringBatchSample2/src/test/java/com/example/spring/SpringBatchSample2ApplicationTests.java
@@ -2,15 +2,22 @@ package com.example.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.spring.repository.SampleTableRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBatchSample2ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
-
+  @Autowired
+  private SampleTableRepository sampleTableRepo;
+  
+  @Test
+  public void contextLoads() {
+    System.out.println(" -- " + new Exception().getStackTrace()[0].getMethodName());
+    System.out.println(sampleTableRepo.findAll().size());
+  }
 }
