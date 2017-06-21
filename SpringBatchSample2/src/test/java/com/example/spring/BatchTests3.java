a118d3b07a74eeabbcdfc7765808a6ee308b597e
Takuya Horimatsu <new_woory@hotmail.com>
2017-06-21 20:22:14 +0900

    フォーマット修正

--- SpringBatchSample2/src/test/java/com/example/spring/BatchTests3.java
+++ SpringBatchSample2/src/test/java/com/example/spring/BatchTests3.java
@@ -8,11 +8,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -30,6 +25,7 @@ public class BatchTests3 {
   @Before
   public void setup() {
   }
+  
   @Test
   public void contextLoads() {
     System.out.println(sampleTableRepo.findAll().size());
--- SpringBatchSample2/src/test/resources/db-schema.sql
+++ SpringBatchSample2/src/test/resources/db-schema.sql
@@ -1,8 +1,7 @@
-
 CREATE TABLE IF NOT EXISTS `sample_table` (
 `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `status` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
-ALTER TABLE `sample_table`
- ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `sample_table` ADD PRIMARY KEY (`id`);
